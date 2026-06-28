931afda7ba5098ba373902b274b7c9a319aa559a
Pedro Ríos G. <69609154+pedroriosg@users.noreply.github.com>
2026-06-27 21:59:47 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE = 'im-tracker-v1';
+const ASSETS = [
+  '/ironman-tracker/',
+  '/ironman-tracker/index.html',
+  '/ironman-tracker/manifest.json',
+  '/ironman-tracker/icon.png',
+  '/ironman-tracker/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS.filter(a => !a.includes('icon'))))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fresh = fetch(e.request).then(res => {
+        if (res.ok) {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return res;
+      }).catch(() => cached);
+      return cached || fresh;
+    })
+  );
+});
